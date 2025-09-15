693c4bf013b85d019aacb0871d804a80458457f2
lpiccirilli <nuclei-secchiata.0k@gmail.com>
2025-09-15 17:58:03 +0200

    god did

--- requirements/mariadb/entrypoint.sh
+++ requirements/mariadb/entrypoint.sh
@@ -1,17 +1,34 @@
-service mariadb start
+#!/bin/bash
 
-sleep 10
+mariadbd-safe --skip-networking &
+pid="$!"
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${db_name}\`;"
+until mariadb -u root -e "SELECT 1;" >/dev/null 2>&1; do
+    sleep 2
+done
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${db_user}\`@'localhost' IDENTIFIED BY '${db_pwd}';"
+# Create database
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${WORDPRESS_DB_NAME}\`;"
 
-mariadb -e "GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO \`${db_user}\`@'%' IDENTIFIED BY '${db_pwd}';"
+# Create user for both localhost and remote connections
+mariadb -e "CREATE USER IF NOT EXISTS \`${WORDPRESS_DB_USER}\`@'localhost' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${WORDPRESS_DB_USER}\`@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
 
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Grant privileges to both user instances
+mariadb -e "GRANT ALL PRIVILEGES ON \`${WORDPRESS_DB_NAME}\`.* TO \`${WORDPRESS_DB_USER}\`@'localhost';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${WORDPRESS_DB_NAME}\`.* TO \`${WORDPRESS_DB_USER}\`@'%';"
 
-mariadb -p1234 -e "FLUSH PRIVILEGES;"
+# Flush privileges before changing root password
+mariadb -e "FLUSH PRIVILEGES;"
 
-mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
+# Change root password (do this last)
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
+# Final flush with new root password
+mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
+# Shutdown MariaDB service
+mariadb-admin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+# Start MariaDB in safe mode
 exec mariadbd-safe
--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -4,6 +4,12 @@ sleep 5s
 
 cd /var/www/html
 
+until nc -z "$WORDPRESS_DB_HOST" 3306; do
+  echo "Waiting for MariaDB..."
+  sleep 5
+done
+
+
 if [ ! -f wp-cli.phar ]; then
   curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
@@ -13,8 +19,20 @@ if [ ! -d "wp-includes" ]; then
   ./wp-cli.phar core download --allow-root
 fi
 
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar config create --allow-root \
+    --dbname=$WORDPRESS_DB_NAME \
+    --dbuser=$WORDPRESS_DB_USER \
+    --dbpass=$WORDPRESS_DB_PASSWORD \
+    --dbhost=$WORDPRESS_DB_HOST:3306 \
+    --path='/var/www/html'
 
-./wp-cli.phar core install --url=https://lpicciri.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install \
+    --url=$DOMAIN_NAME/ \
+	--title=$WP_SITE_TITLE \
+    --admin_user=$WP_ADMIN_USR \
+    --admin_password=$WP_ADMIN_PWD \
+    --admin_email=$WP_ADMIN_EMAIL \
+    --skip-email \
+    --allow-root
 
-php-fpm8.4 -F
+/usr/sbin/php-fpm7.4 -F
